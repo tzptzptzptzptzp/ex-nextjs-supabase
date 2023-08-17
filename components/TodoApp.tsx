ca84e3689bda076fc1a5e6bf44112176cf536afc
tzptzptzptzptzp <tzp2539@gmail.com>
2023-08-17 23:53:11 +0900

    ✨ フォームを作成

--- components/TodoApp.tsx
+++ components/TodoApp.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 export function TodoApp() {
   return (
     <div>
-      <h2>Next.js & SupaBase</h2>
+      <h2 className='mb-2 text-2xl text-center'>Next.js & SupaBase</h2>
+      <form>
+        <input type="text" className='shadow-lg p-2 outline-none' />
+        <button className='shadow-md bottom-2 p-2 rounded-md'>Add</button>
+      </form>
     </div>
   )
 }

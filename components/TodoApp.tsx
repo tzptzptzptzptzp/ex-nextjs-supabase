a6496e91a99f7631ec7182a57ed98e2c35eafc13
tzptzptzptzptzp <tzp2539@gmail.com>
2023-08-20 00:07:45 +0900

    ✨ データを追加する機能を実装

--- components/TodoApp.tsx
+++ components/TodoApp.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { TodoList } from './TodoList'
-import { getAllTodos } from '../utils/supabaseFunctions'
+import { addTodo, getAllTodos } from '../utils/supabaseFunctions'
 import { Todo } from '../utils/interface'
 
 export function TodoApp() {
   const [todos, setTodos] = useState<Todo[]>([])
+  const [title, setTitle] = useState<string>('')
 
   useEffect(() => {
     const getTodos = async () => {
@@ -14,11 +15,18 @@ export function TodoApp() {
     getTodos()
   }, [])
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (title === '') return
+    await addTodo(title)
+    setTitle('')
+  }
+
   return (
     <div>
       <h2 className='mb-2 text-2xl text-center'>Next.js & SupaBase</h2>
-      <form className='mb-4'>
-        <input type="text" className='shadow-lg p-2 outline-none' />
+      <form onSubmit={(e) => handleSubmit(e)} className='mb-4'>
+        <input onChange={(e) => setTitle(e.target.value)} value={title} type="text" className='shadow-lg p-2 outline-none' />
         <button className='shadow-md bottom-2 p-2 rounded-md'>Add</button>
       </form>
       <TodoList todos={todos}></TodoList>
--- utils/supabaseFunctions.ts
+++ utils/supabaseFunctions.ts
@@ -4,3 +4,7 @@ export const getAllTodos = async () => {
   const todos = await supabase.from("todo").select("*");
   return todos.data;
 };
+
+export const addTodo = async (title: string) => {
+  await supabase.from("todo").insert({ title: title });
+};
